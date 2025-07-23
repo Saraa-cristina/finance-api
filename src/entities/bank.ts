48eb5da4345276af938b741115c9e2be2c59fc55
Sara Cristina <saracristinacs09@gmail.com>
2025-07-22 20:29:54 -0400

    Update bank.ts

---
+++ src/entities/bank.ts
@@ -0,0 +1,26 @@
+import { randomUUID } from "node:crypto";
+
+
+type BankType = 'income' | 'expense';
+
+export class Entity {
+  id: string;
+  updatedAt: Date | null;
+  constructor(id?: string, updatedAt?: Date | null) {
+    this.id = id || randomUUID();
+    this.updatedAt = updatedAt || null;
+  }
+}
+
+
+
+export class Bank extends Entity {
+  icon?: string | null;
+  name: string;
+  constructor(name: string, icon?: string | null, id?: string, updatedAt?: Date | null) {
+    super(id, updatedAt);
+    this.icon = icon;
+    this.name = name;
+  }
+}
+
